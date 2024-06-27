a9b147a54eabe511f1c9478986b4b070a692bea0
VieroFamico <viero.famico@gmail.com>
2024-06-27 11:15:01 +0700

    a

--- Take Me to The Water/Assets/Scripts/Managers/GameManager.cs
+++ Take Me to The Water/Assets/Scripts/Managers/GameManager.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Services.Economy;
 using UnityEngine;
-using static PlayerLoadout;
-using static StylizedWater2.WaterMesh;
 
 public class GameManager : MonoBehaviour
 {
